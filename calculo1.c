936d33580b061b81dc6cb7e81d6afb3611fde8db
jerryapp23 <jerrygithub.app@gmail.com>
2026-01-28 23:26:19 -0300

    Atualizar o calculo1.c

--- calculo1.c
+++ calculo1.c
@@ -6,29 +6,12 @@ int main(){
     int soma = a + b;
     int diferenca = a - b;
     int produto = a * b;
-    int quoeficiente = a/b; // note que a divisão de inteiro resulta em um número inteiro
+    int quoeciente = a/b; // note que a divisão de inteiro resulta em um número inteiro
     
-    printf("Soma: %\n", soma);
-    printf("Diferenca: %\n", diferenca);
-    printf("Produto: %\n", produto);
-    printf("Quoeficiente: %\n", quoeficiete);
+    printf("Soma: %d\n", soma);
+    printf("Diferenca: %d\n", diferenca);
+    printf("Produto: %d\n", produto);
+    printf("quociente: %d\n", quociente);
     
 return 0;
 
-#include <stdio.h>
- 
-int main() {
-    int a = 10;
-    int b = 3;
-    int soma = a + b;
-    int diferenca = a - b;
-    int produto = a * b;
-    int quociente = a / b; // Note que a divisão de inteiros resulta em um número inteiro
- 
-    printf("Soma: %d\n", soma);
-    printf("Diferença: %d\n", diferenca);
-    printf("Produto: %d\n", produto);
-    printf("Quociente: %d\n", quociente);
- 
-    return 0;
-}
