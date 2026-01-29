094bfc1f8aa235c5e7cd48395d9ec1e0c6b338b7
jerryapp23 <jerrygithub.app@gmail.com>
2026-01-28 21:01:28 -0300

    Criar calculo1.c

---
+++ calculo1.c
@@ -0,0 +1,16 @@
+#incluse <stdio.h>
+
+int main(){
+    int a = 10;
+    int b = 3;
+    int soma = a + b;
+    int diferenca = a - b;
+    int produto = a * b;
+    int quoeficiente = a/b; // note que a divisão de inteiro resulta em um número inteiro
+    
+    printf("Soma: %/n", soma);
+    printf("Diferenca: %/n", diferenca):
+    
+    
+
+return 0;
