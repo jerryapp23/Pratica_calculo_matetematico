1496e509452d30d508090da4be377b81dfefaa68
jerryapp23 <jerrygithub.app@gmail.com>
2026-01-29 07:03:01 -0300

    Atualizar o calculomedia.c

--- calculomedia.c
+++ calculomedia.c
@@ -1,10 +1,23 @@
 #include <stdio.h>
 
 int main() {
+    //programa para calcular média com numeros inteiros == (int) e (%d)
+    
+    int nota1, nota2, nota3;
+    int media;
     
     printf("=== PROGRAMA PARA CALCULAR MEDIA ===\n");
     
     printf("Digite a priemeira nota: \n");
+    scanf(%d, &nota1");
+    
+    printf("Digite a Segunda nota: \n");
+    scanf(%d, &nota2");
+    
+    printf("Digite a Trrceira nota: \n");
+    scanf(%d, &nota3");
+    
+    media = (nota1 + nota2 + nota3) /3;
     
 return 0;
 }
