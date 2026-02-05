e8995ef8718c385292a093ae15c9de5fb0638ca5
jerryapp23 <jerrygithub.app@gmail.com>
2026-02-05 20:29:01 -0300

    Refactor media calculation to use float variables

--- calculomedia.c
+++ calculomedia.c
@@ -1,23 +1,23 @@
 #include <stdio.h>
 
-int main() {
-    //programa para calcular média com numeros inteiros == (int) e (%d)
+int main(){
+    float nota1, nota2, nota3;
+    float media;
+
+    printf("*** Programa de Cálculo de Média ***\n");
+
+    printf("Digite a sua primeira nota: \n");
+    scanf("%f", &nota1);
+ 
+    printf("Digite a sua segunda nota: \n");
+    scanf("%f", &nota2);
+
+    printf("Digite a sua terceria nota: \n");
+    scanf("%f", &nota3);
+
+    media = (nota1 + nota2 + nota3) / 3 ;
+
+    printf(" A média é: %.1f", media);
     
-    int nota1, nota2, nota3;
-    int media;
-    
-    printf("=== PROGRAMA PARA CALCULAR MEDIA ===\n");
-    
-    printf("Digite a priemeira nota: \n");
-    scanf(%d, &nota1");
-    
-    printf("Digite a Segunda nota: \n");
-    scanf(%d, &nota2");
-    
-    printf("Digite a Trrceira nota: \n");
-    scanf(%d, &nota3");
-    
-    media = (nota1 + nota2 + nota3) /3;
-    
-return 0;
+    return 0;
 }
